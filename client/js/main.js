04cf202e6c34f8fd53f62e5c1c0dc9fadad5e1dd
Jaime Liz <jaime.liz@ksquarelabs.com>
2016-10-20 14:19:58 -0500

    add initial table loader

--- client/js/main.js
+++ client/js/main.js
@@ -7,7 +7,8 @@ require.config({
         handsontable: "../lib/handsontable/dist/handsontable.full",
         moment: "../lib/moment/moment",
         pikaday: "../lib/pikaday/pikaday",
-        zeroclipboard: "../lib/zeroclipboard/dist/ZeroClipboard"
+        zeroclipboard: "../lib/zeroclipboard/dist/ZeroClipboard",
+        chart: "../lib/chart.js/dist/Chart"
     },
     shim: {
         "handsontable": {
@@ -17,21 +18,17 @@ require.config({
     }
 });
 
-define(["handsontable"], function(Handsontable){
-
+define(["handsontable", "chart", "handlers", "tableCtrl"], function (Handsontable, Chart, handlers, tableCtrl) {
+    //initial data
     var data = [
-        ["", "Ford", "Volvo", "Toyota", "Honda"],
-        ["2016", 10, 11, 12, 13],
-        ["2017", 20, 11, 14, 13],
-        ["2018", 30, 15, 12, 13]
+        ["", "Honda", "Toyota", "Volkswagen", "Ford"],
+        ["2016", 100, 23, 66, 1],
+        ["2017", 123, 11, 11, 1],
+        ["2018", 30, 30, 15, 1]
     ];
-
-    var container = document.getElementById('table');
-    var hot = new Handsontable(container, {
-        data: data,
-        rowHeaders: true,
-        colHeaders: true,
-        dropdownMenu: true
+    var container = document.getElementById('table'); // default container
+    tableCtrl.load(container,data, function (ch, src) {
+        //Handle this business
     });
 
 
