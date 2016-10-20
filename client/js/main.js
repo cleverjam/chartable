e3089a8172736cc5d626950dfcab2d0d50b79d3c
Jaime Liz <jaime.liz@ksquarelabs.com>
2016-10-20 16:16:17 -0500

    implement chart controller in the main module

--- client/js/main.js
+++ client/js/main.js
@@ -18,17 +18,21 @@ require.config({
     }
 });
 
-define(["handsontable", "chart", "handlers", "tableCtrl"], function (Handsontable, Chart, handlers, tableCtrl) {
+define(["handsontable", "chart", "handlers", "tableCtrl", "chartCtrl"], function (Handsontable, Chart, handlers, tableCtrl, chartCtrl) {
+    var ctx = document.getElementById("myChart");
+    var container = document.getElementById('table'); // default container
     //initial data
     var data = [
-        ["", "Honda", "Toyota", "Volkswagen", "Ford"],
-        ["2016", 100, 23, 66, 1],
-        ["2017", 123, 11, 11, 1],
-        ["2018", 30, 30, 15, 1]
+        ["", "Honda", "Toyota", "Volkswagen", "Ford", "Ferrari"],
+        ["2016", 100, 23, 66, 1, 2],
+        ["2017", 123, 11, 11, 1, 2],
+        ["2018", 30, 30, 15, 1, 2],
+        ["2019", 15, 12, 22, 3, 6]
     ];
-    var container = document.getElementById('table'); // default container
+    var chartObj = chartCtrl.load(ctx, "line", data);
     tableCtrl.load(container,data, function (ch, src) {
         //Handle this business
+        console.log(ch, src, chartObj)
     });
 
 
